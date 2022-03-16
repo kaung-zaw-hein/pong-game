2a4996bb9bc094bed8ff59cf2fdf2f128ea8522e
Kaung Zaw Hein <kaungzawhein1963@gmail.com>
2022-03-16 11:52:39 +0630

    with express

--- server.js
+++ server.js
@@ -1,41 +1,21 @@
-const server = require('http').createServer();
+const http = require('http');
+const io = require('socket.io');
 
-const io = require('socket.io')(server, {
-  cors: {
-    origin: '*',
-    methods: ['GET', 'POST']
-  }
-});
+const apiServer = require('./api');
+const httpServer = http.createServer(apiServer);
+const socketServer = io(httpServer);
+
+// const io = require('socket.io')(server, {
+//   cors: {
+//     origin: '*',
+//     methods: ['GET', 'POST']
+//   }
+// });
+
+const sockets = require('./socket');
 
 const PORT = 3000;
-
-server.listen(PORT);
+httpServer.listen(PORT);
 console.log(`Listening on port ${PORT}...`);
 
-let readyPlayerCount = 0;
-
-io.on('connection', (socket) => {
-  console.log('a user connected', socket.id);
-
-  socket.on('ready', () => {
-    console.log('Player ready', socket.id);
-    
-      readyPlayerCount++;
-
-      if(readyPlayerCount % 2 === 0){
-        io.emit('startGame', socket.id);
-      }
-  });
-
-  socket.on('paddleMove', (paddleData) => {
-    socket.broadcast.emit('paddleMove', paddleData);
-  });
-
-  socket.on('ballMove', (ballData) => {
-    socket.broadcast.emit('ballMove', ballData);
-  });
-
-  socket.on('disconnect', (reason) => {
-    console.log(`Client ${socket.id} disconnected: ${reason}`);
-  })
-});
+sockets.listen(socketServer);
